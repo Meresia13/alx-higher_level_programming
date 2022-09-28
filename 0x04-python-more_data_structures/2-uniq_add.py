56adced24ead717becc3004f005007fad7fd5b3a
Meresia13 <melisaopiyo@gmail.com>
2022-09-28 13:23:44 -0700

    data structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -8,7 +8,8 @@ def uniq_add(my_list=[]):
         return None
     return sum(set(my_list))
 
+
 if __name__ == '__main__':
     my_list = [1, 2, 3, 1, 4, 2, 5]
     result = uniq_add(my_list)
-    print("Result: {:d}".format(result))
+    print("Result: {:d}".format(result)i)
