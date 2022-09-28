89441b8eb9aa275aa12aa8e41336ebb42d5a617d
Meresia13 <melisaopiyo@gmail.com>
2022-09-28 13:38:47 -0700

    data structures

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
     set_1 = {"Python", "C", "Javascript"}
     set_2 = {"Bash", "C", "Ruby", "Perl"}
     od_set = only_diff_elements(set_1, set_2)
-    print(sorted(list(od_set))i)
+    print(sorted(list(od_set)))
