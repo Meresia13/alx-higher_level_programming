c5f62b67f0c6a8f49fec08bc82fd0872222f39c3
Meresia13 <melisaopiyo@gmail.com>
2022-09-28 12:10:08 -0700

    loops and functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-i = 122
-while i >= 97:
-    flag = 0
-    if i % 2 != 0:
-        i = i - 32
-        flag = 1
-    print("{:s}".format(chr(i)), end="")
-    if flag == 1:
-        i = i + 32
-    i = i - 1
+print("".join(["{:c}".format(c-32 if c % 2 else c)
+    for c in range(122, 96, -1)]), end="")
