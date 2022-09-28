dd01d249cc658a541fa13a580368f46d13246a2a
Meresia13 <melisaopiyo@gmail.com>
2022-09-28 13:28:25 -0700

    data structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -8,7 +8,6 @@ def uniq_add(my_list=[]):
         return None
     return sum(set(my_list))
 
-
 if __name__ == '__main__':
     my_list = [1, 2, 3, 1, 4, 2, 5]
     result = uniq_add(my_list)
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -9,6 +9,7 @@ def common_elements(set_1, set_2):
     """
     return set_1.intersection(set_2)
 
+
 if __name__ == '__main__':
     set_1 = {"Python", "C", "Javascript"}
     set_2 = {"Bash", "C", "Ruby", "Perl"}
